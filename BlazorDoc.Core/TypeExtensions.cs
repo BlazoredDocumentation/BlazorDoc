cc9834d93e4f68d128fd78a85a794abc5336bd99
BlazoredDocumentation <BlazoredDocumentation@outlook.de>
2021-05-19 23:32:48 +0200

    Added ArraySupport for Nameconverting

--- BlazorDoc.Core/TypeExtensions.cs
+++ BlazorDoc.Core/TypeExtensions.cs
@@ -7,17 +7,17 @@ namespace BlazorDoc.Components
     public static class TypeExtensions
     {
         private static Dictionary<string, string> _typeNameMap => new Dictionary<string, string>(){
-            {typeof(bool).Name,"bool"},
-            {typeof(int).Name,"int"},
-            {typeof(short).Name,"short"},
-            {typeof(long).Name,"long"},
-            {typeof(string).Name,"string" },
+            {typeof(bool).Name,"bool"},{typeof(bool[]).Name,"bool[]"},
+            {typeof(int).Name,"int"},{typeof(int[]).Name,"int[]"},
+            {typeof(short).Name,"short"}, {typeof(short[]).Name,"short[]"},
+            {typeof(long).Name,"long"},  {typeof(long[]).Name,"long[]"},
+            {typeof(string).Name,"string" },{typeof(string[]).Name,"string[]" },
             {typeof(void).Name,"void" },
-            {typeof(char).Name,"char" },
-            {typeof(decimal).Name,"decimal" },
-            {typeof(double).Name,"double"}
+            {typeof(char).Name,"char" },{typeof(char[]).Name,"char[]" },
+            {typeof(decimal).Name,"decimal" }, {typeof(decimal[]).Name,"decimal[]" },
+            {typeof(double).Name,"double"}, {typeof(double[]).Name,"double[]"}
             };
- 
+
         public static string GetHumanIntrestingTypeName(this Type type)
         {
             string typeName = type.Name;
